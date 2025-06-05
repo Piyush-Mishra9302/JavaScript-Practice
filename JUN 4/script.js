dd033b0e06bd25626caa0f6184c9acba9e5f9871
Piyush Mishra <piyushmis9302@gmail.com>
2025-06-05 23:58:00 +0530

    Practice

--- JUN 4/script.js
+++ JUN 4/script.js
@@ -18,10 +18,19 @@ console.log("June 4");
 //     alert("I am from function 1")
 // }
 
-let hello = () =>{
-   let ptag = document.getElementById("text");
-   ptag.innerHTML = "<h1>Hello World</h1>"
+// let hello = () =>{
+//    let ptag = document.getElementById("text");
+//    ptag.innerHTML = "<h1>Hello World</h1>"
    
 
-}
+// }
 
+let first = document.getElementById("1st");
+first.innerHTML = "<h1>Piyush</h1>"
+
+let second = document.getElementById("2nd");
+second.innerHTML = "<h1>Mishra</h1>"
+second.style.textDecoration = "underline"
+
+let Third = document.getElementById("3rd");
+Third.innerHTML = "<h1>From js</h1>"
