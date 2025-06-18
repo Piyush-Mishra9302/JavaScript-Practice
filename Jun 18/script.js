8cf6aa133f77d60dcb7367eb5f213944d72192ab
Piyush Mishra <piyushmis9302@gmail.com>
2025-06-18 19:05:25 +0530

    Learning js

---
+++ Jun 18/script.js
@@ -0,0 +1,9 @@
+
+let Time = new Date();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+
+console.log(days[Time.getDay()]);
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "AUg", "sep", "Oct", "Nov", "Dec"];
+console.log(months[Time.getMonth()]);
