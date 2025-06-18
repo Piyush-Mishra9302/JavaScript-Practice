5ce7bffb3c2ebfbfd96ba0c67db63b0297647f06
Piyush Mishra <piyushmis9302@gmail.com>
2025-06-18 19:32:10 +0530

    Learning js

--- Jun 18/script.js
+++ Jun 18/script.js
@@ -1,9 +1,45 @@
 
-let Time = new Date();
+// let Time = new Date();
 
-let days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+// let days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
 
-console.log(days[Time.getDay()]);
+// console.log(days[Time.getDay()]);
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "AUg", "sep", "Oct", "Nov", "Dec"];
-console.log(months[Time.getMonth()]);
+// let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "AUg", "sep", "Oct", "Nov", "Dec"];
+// console.log(months[Time.getMonth()]);
+
+// let counter = document.getElementById("count")
+//  let count = 0 ;
+// let inccount = () => {
+   
+//     console.log(count);
+//     count++;
+// }
+
+
+
+// let startCount = () => {
+   
+//   setInterval(inccount, 2000);
+//  } 
+
+// let stop = () => {
+//     clearInterval(inccount);
+// }
+
+let count = 0;
+  let interval; 
+
+  let inccount = () => {
+    console.log(count);
+    count++;
+  };
+
+  let startCount = () => {
+    interval = setInterval(inccount, 2000); 
+  };
+
+  let stop = () => {
+    clearInterval(interval); 
+  };
+
