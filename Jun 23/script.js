a0d4d13e6ccb4b0ec12db7b185a31a51127a9155
Piyush Mishra <piyushmis9302@gmail.com>
2025-06-23 22:49:31 +0530

    Problem Solving

---
+++ Jun 23/script.js
@@ -0,0 +1,8 @@
+//Js programe to reverse a number
+
+let number  = 4567;
+
+let reverse = Number (number.toString().split('').reverse().join(''));
+
+console.log(reverse);
+console.log(typeof reverse);
