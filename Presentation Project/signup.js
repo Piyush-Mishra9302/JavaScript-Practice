7a96e0d108fdf9d7136ce5cb007f57e9b77f3e23
Piyush Mishra <piyushmis9302@gmail.com>
2025-06-25 23:31:37 +0530

    Started movie ticket booking app

---
+++ Presentation Project/signup.js
@@ -0,0 +1,7 @@
+let UserName = document.querySelector("#username").value; 
+let Number = document.querySelector("#number").value; 
+let Email = document.querySelector("#email").value; 
+let Password = document.querySelector("#password").value; 
+// let Cpassword = document.querySelector("cpassword").value; 
+
+
